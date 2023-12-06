9b1e7f833b3baef722dc0774bb6b0ca5fb6ee5ec
Alex <kaf-2012@mail.ru>
2023-12-06 23:08:53 +0300

    modified

--- pars.py
+++ pars.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import pandas as pd
 
-url = "https://www.perekrestok.ru/cat/d"
+url = "https://www.perekrestok.ru/cat/mc/1066/vse-dla-novogo-goda"
 
 # Отправляем GET-запрос к странице
 response = requests.get(url)
@@ -15,18 +15,22 @@ if response.status_code == 200:
     # Находим интересующие нас элементы на странице
     products = soup.find_all('div', class_='product-card__title')
 
-    # Создаем CSV-файл для записи данных
-    with open('products.csv', mode='w', encoding='utf-16', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Товар', 'Цена'])
+    # Создаем список для хранения данных
+    data = []
 
-        # Выводим найденные названия продуктов и цены, записываем их в CSV
-        for product in products:
-            product_name = product.text.strip()
-            product_price_container = product.find_next('div', class_='product-card__price')
-            product_price = product_price_container.find('div', class_='price-new').text.strip() if product_price_container else "Цена не указана"
-            print(f"Товар: {product_name}, Цена: {product_price}")
-            writer.writerow([product_name, product_price])
+    # Выводим найденные названия продуктов и цены, записываем их в список
+    for product in products:
+        product_name = product.text.strip()
+        product_price_container = product.find_next('div', class_='product-card__price')
+        product_price = product_price_container.find('div', class_='price-new').text.strip().replace('\xa0', '') if product_price_container else "Цена не указана"
+        print(f"Товар: {product_name}, Цена: {product_price}")
+        data.append([product_name, product_price])
+
+    # Создаем DataFrame из списка данных
+    df = pd.DataFrame(data, columns=['Товар', 'Цена'])
+
+    # Записываем DataFrame в CSV-файл
+    df.to_csv('products.csv', encoding='utf-16', index=False, sep='\t')
 
     print("Данные успешно записаны в products.csv")
 else:
