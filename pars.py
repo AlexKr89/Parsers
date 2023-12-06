31aaef72f959b8c1ac2ea017418f55544e17d7ca
Alex <kaf-2012@mail.ru>
2023-12-06 23:52:01 +0300

    modified

--- pars.py
+++ pars.py
@@ -4,21 +4,16 @@ import pandas as pd
 
 url = "https://www.perekrestok.ru/cat/mc/1066/vse-dla-novogo-goda"
 
-# Отправляем GET-запрос к странице
 response = requests.get(url)
 
-# Проверяем успешность запроса
 if response.status_code == 200:
-    # Используем BeautifulSoup для парсинга HTML-страницы
+
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Находим интересующие нас элементы на странице
     products = soup.find_all('div', class_='product-card__title')
 
-    # Создаем список для хранения данных
     data = []
 
-    # Выводим найденные названия продуктов и цены, записываем их в список
     for product in products:
         product_name = product.text.strip()
         product_price_container = product.find_next('div', class_='product-card__price')
@@ -26,10 +21,8 @@ if response.status_code == 200:
         print(f"Товар: {product_name}, Цена: {product_price}")
         data.append([product_name, product_price])
 
-    # Создаем DataFrame из списка данных
     df = pd.DataFrame(data, columns=['Товар', 'Цена'])
 
-    # Записываем DataFrame в CSV-файл
     df.to_csv('products.csv', encoding='utf-16', index=False, sep='\t')
 
     print("Данные успешно записаны в products.csv")
